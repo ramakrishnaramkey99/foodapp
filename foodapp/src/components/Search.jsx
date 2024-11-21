94cb54dede2f23f2be125ab7b00b1be1ac4e0e04
Ram Krishna <mkrishna@alphavsolutions.com>
2024-11-21 23:54:41 +0530

    50to55

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
-
-export default function Search() {
+import styles from "./search.module.css";
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "bc45cde4d6364dd1b0176ccdc1b3fb0d";
+export default function Search({ foodData, setFoodData }) {
   const [query, SetQuery] = useState("Pizza");
 
   useEffect(() => {
-    function demo() {
-      console.log("Demo Function executed");
+    async function fetchfood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
     }
-    demo();
-  }, []);
+    fetchfood();
+  }, [query]);
   return (
-    <div>
+    <div className={styles.searchContainer}>
       <input
+        className={styles.input}
         type="text"
         value={query}
         onChange={(e) => SetQuery(e.target.value)}
       />
-      <button>HI</button>
     </div>
   );
 }
