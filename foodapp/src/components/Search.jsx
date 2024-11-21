ca1abdef7831f7e4b79c102d25b35b85761a686a
Ram Krishna <mkrishna@alphavsolutions.com>
2024-11-21 14:07:30 +0530

    Foodapp

---
+++ foodapp/src/components/Search.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function Search() {
+  const [query, SetQuery] = useState("Pizza");
+
+  useEffect(() => {
+    function demo() {
+      console.log("Demo Function executed");
+    }
+    demo();
+  }, []);
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => SetQuery(e.target.value)}
+      />
+    </div>
+  );
+}
